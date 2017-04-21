cf07f21d3e9419d35e67f24de323cfc21cc68534
unknown <arcDaniel@users.noreply.github.com>
2017-04-21 14:28:45 -0500

    Commited

---
+++ hello.js
@@ -0,0 +1,6 @@
+var http = require("http");
+http.createServer(function(request,response){
+	response.writeHead(200,{"Content-Type":"text/plain"});
+	response.end('Hello World!\n');
+}).listen(8082);
+console.log("Server runing at port 8082");
