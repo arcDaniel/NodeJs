1536364a208dabc4fca83949349fc8d913120f3d
unknown <arcDaniel@users.noreply.github.com>
2017-05-08 17:42:58 -0500

    Commited

--- hello.js
+++ hello.js
@@ -7,6 +7,7 @@ http.createServer(function(request,response){
 }).listen(8082);
 console.log("Server runing at port 8082");
 */
+
 // INICIALIZACIÓN DE SERVIDOR CON EXPRESS.JS
 var express = require('express');
 var app = express();
@@ -22,3 +23,17 @@ app.use(function(req, res, next) {
 app.listen(8082, function () {
   console.log('Example app listening on port 8082!');
 });
+
+// LEER FICHEROS
+// Code Blocking
+var fs = require("fs");
+var data = fs.readFileSync('public/input.txt');
+console.log(data.toString());
+console.log("program Ended");
+// Non Blocking Code
+fs.readFile('public/input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+
+console.log("Program Ended 2");
